bf3954d80f12ec8f63a1f51f7f46cbb0f9d98161
Théo Szymkowiak <theoszymko@me.com>
2017-06-12 17:27:09 -0400

    Adding clay runtime

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# clay-runtime
+# Clay Runtime
+
+Provides a simple to use interface for lambda functions.
+
+Currently supports:
+- Amazon Web Services Lambda
+
+## Install
+
+```
+$ npm install clay-runtime --save
+```
+
+Then use the starter code:
+
+```js
+// Require your files or libraries here. You can use npm to install libraries.
+var clay = require('./clay.js');
+
+exports.handler = function() {
+    // Has to be the first line of the function
+    clay.start()
+
+    /*
+    Your service only responds to POST requests
+    any variables passed are found in event.body
+    eventVars is a convenience that parses any JSON
+    objects that were passed in the POST request
+    */
+
+    var key1 = clay.params.key1;
+
+    /*
+    JSON Stringify the result of the service call
+    In this example we simply pass back whatever parameters
+    were sent to the service
+    */
+
+    clay.status(200)
+        .header('Header', 'value')
+        .end(clay.params);
+}
+```
+
+## Documentation
+
+Soon to come! Go to [Clay.run](https://clay.run) to learn more about Lambda functions.
---
+++ index.js
@@ -0,0 +1,78 @@
+/**
+ * Clay runtime for cloud functions.
+ */
+
+function startFunction() {
+    this.event = startFunction.caller.arguments[0]
+    this.context = startFunction.caller.arguments[1]
+    this.callback = startFunction.caller.arguments[2]
+
+    try { 
+        this.params = JSON.parse(this.event.body);
+    } catch(e) {
+        // Not json input
+        console.log(e);
+    }
+}
+
+var clayRuntime = {
+    event: null,
+    context: null,
+    callback: null,
+
+    // Default status code
+    statusCode: 200,
+
+    // Default headers
+    headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+
+    start: startFunction.bind(this),
+
+    /**
+     * Set the status of the lambda
+     * @param {int} status
+     */
+    status: function(status) {
+        this.statusCode = status;
+        return this;
+    },
+
+    /**
+     * Add or modify a header
+     * @param {string} name
+     * @param {string} value
+     */
+    header: function(name, value) {
+        this.headers[name] = value;
+        return this;
+    },
+
+    /**
+     * End the lambda function with a result
+     * @param {result} can be json or error
+     */
+    end: function(result) {
+        if(result instanceof Error) {
+            return this.callback(result);
+        }
+
+        return this.context.succeed({
+            'body': JSON.stringify(result),
+            'statusCode': this.statusCode,
+            'headers': this.headers
+        })
+    },
+
+    /**
+     * Returns the base dir where the function
+     * can write and read files.
+     */
+    baseDir: function() {
+        return '/tmp/';
+    }
+}
+
+module.exports = clay;
